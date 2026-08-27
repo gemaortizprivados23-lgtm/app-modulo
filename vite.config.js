c2ea5708638ddc13dbbd18b9693c3115f16b20c6
gemaortizprivados23-lgtm <gemaortizprivados23@clases.edu.sv>
2026-08-27 09:50:15 -0600

    Corregir despliegue GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// GitHub Pages publica la aplicación dentro de /app-modulo/
 export default defineConfig({
-  base: '/app-modulo/',
   plugins: [react()],
+  base: '/app-modulo/'
 })
